f0608db559109fd56fab301d0795d54879180d23
Aidan O Brien <50273228+obrien-aidan@users.noreply.github.com>
2021-01-02 18:42:20 +0000

    F5 - rateHillfort ----------------------------------- - add rating of hillfort on create / edit - rating show on hillfort list ----------------------------------- to fix- hillfort_activity.xml constraints

--- app/src/main/java/org/wit/hillfort/views/hillfort/HillfortView.kt
+++ app/src/main/java/org/wit/hillfort/views/hillfort/HillfortView.kt
@@ -7,13 +7,16 @@ import android.view.MenuItem
 import com.bumptech.glide.Glide
 import com.google.android.gms.maps.GoogleMap
 import kotlinx.android.synthetic.main.activity_hillfort.*
+import kotlinx.android.synthetic.main.activity_hillfort_list.*
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.hillfort.R
 import org.wit.hillfort.helpers.readImageFromPath
 import org.wit.hillfort.models.HillfortModel
 import org.wit.hillfort.models.Location
 import org.wit.hillfort.views.BaseView
+
 
 class HillfortView : BaseView(), AnkoLogger {
 
@@ -37,7 +40,7 @@ class HillfortView : BaseView(), AnkoLogger {
         presenter = initPresenter (HillfortPresenter(this)) as HillfortPresenter
 
         chooseImage.setOnClickListener {
-            presenter.cacheHillfort(hillfortTitle.text.toString(), description.text.toString())
+            presenter.cacheHillfort(hillfortTitle.text.toString(), description.text.toString(), rating1.rating.toFloat())
             presenter.doSelectImage()
         }
 
@@ -50,6 +53,7 @@ class HillfortView : BaseView(), AnkoLogger {
     override fun showHillfort(hillfort: HillfortModel) {
         if (hillfortTitle.text.isEmpty()) hillfortTitle.setText(hillfort.title)
         if (description.text.isEmpty())  description.setText(hillfort.description)
+        rating1.setRating(hillfort.rating)
         Glide.with(this).load(hillfort.image).into(hillfortImage);
 
         if (hillfort.image != null) {
@@ -57,7 +61,6 @@ class HillfortView : BaseView(), AnkoLogger {
         }
         this.showLocation(hillfort.location)
     }
-
     override fun showLocation (loc : Location) {
         lat.setText("%.6f".format(loc.lat))
         lng.setText("%.6f".format(loc.lng))
@@ -69,16 +72,20 @@ class HillfortView : BaseView(), AnkoLogger {
         return super.onCreateOptionsMenu(menu)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+/*    override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item?.itemId) {
             R.id.item_delete -> {
                 presenter.doDelete()
             }
+
             R.id.item_save -> {
-                if (hillfortTitle.text.toString().isEmpty()) {
-                    toast(R.string.enter_hillfort_title)
-                } else {
-                    presenter.doAddOrSave(hillfortTitle.text.toString(), description.text.toString())
+                hillfort.description = description.text.toString()
+                hillfort.rating = rating1.getRating()
+                if (hillfort.title.isEmpty()) {
+                    if (hillfort.title.isEmpty()) {
+                        toast(R.string.enter_hillfort_title)
+                    }
+                    presenter.doAddOrSave(hillfort.title, hillfort.description, hillfort.rating)
                 }
             }
             R.id.item_cancel -> {
@@ -86,7 +93,25 @@ class HillfortView : BaseView(), AnkoLogger {
             }
         }
         return super.onOptionsItemSelected(item)
+    }*/
+override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when (item?.itemId) {
+        R.id.item_delete -> {
+            presenter.doDelete()
+        }
+        R.id.item_save -> {
+            if (hillfortTitle.text.toString().isEmpty()) {
+                toast(R.string.enter_hillfort_title)
+            } else {
+                presenter.doAddOrSave(hillfortTitle.text.toString(), description.text.toString(),rating1.getRating())
+            }
+        }
+        R.id.item_cancel -> {
+            finish()
+        }
     }
+    return super.onOptionsItemSelected(item)
+}
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -124,5 +149,4 @@ class HillfortView : BaseView(), AnkoLogger {
         super.onSaveInstanceState(outState)
         mapView.onSaveInstanceState(outState)
     }
-
 }
